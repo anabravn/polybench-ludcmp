557140868bbdae3dc9806e32957464b54e0e7fe0
a-lvaro <aaflneto@gmail.com>
2023-08-23 14:02:38 -0300

    identação

--- ludcmp.c
+++ ludcmp.c
@@ -70,42 +70,50 @@ void kernel_ludcmp(int n,
   DATA_TYPE w;
 
 #pragma scop
+   // Esse bloco calcula as matrizes L e U
   b[0] = 1.0;
   for (i = 0; i < n; i++)
     {
-      for (j = i+1; j <= n; j++)
-        {
-	  w = A[j][i];
-	  for (k = 0; k < i; k++)
-	    w = w- A[j][k] * A[k][i];
-	  A[j][i] = w / A[i][i];
+      for (j = i+1; j <= n; j++) {
+	      w = A[j][i]; 
+
+	      for (k = 0; k < i; k++)
+	        w = w- A[j][k] * A[k][i];
+
+	      A[j][i] = w / A[i][i]; // Armazena o valor calculado em A[j][i] (matriz L)
         }
-      for (j = i+1; j <= n; j++)
-        {
-	  w = A[i+1][j];
-	  for (k = 0; k <= i; k++)
-	    w = w  - A[i+1][k] * A[k][j];
-	  A[i+1][j] = w;
+
+      for (j = i+1; j <= n; j++) {
+	      w = A[i+1][j];
+
+	      for (k = 0; k <= i; k++)
+	        w = w  - A[i+1][k] * A[k][j];
+
+	      A[i+1][j] = w; // Armazena o valor calculado em A[i+1][j] (matriz U)
         }
     }
+
   y[0] = b[0];
-  for (i = 1; i <= n; i++)
-    {
+  for (i = 1; i <= n; i++) {
       w = b[i];
+
       for (j = 0; j < i; j++)
-	w = w - A[i][j] * y[j];
+	      w = w - A[i][j] * y[j];
+
       y[i] = w;
     }
+
   x[n] = y[n] / A[n][n];
-  for (i = 0; i <= n - 1; i++)
-    {
+  for (i = 0; i <= n - 1; i++) {
       w = y[n - 1 - (i)];
+
       for (j = n - i; j <= n; j++)
-	w = w - A[n - 1 - i][j] * x[j];
+	      w = w - A[n - 1 - i][j] * x[j];
+
       x[n - 1 - i] = w / A[n - 1 - (i)][n - 1-(i)];
     }
-#pragma endscop
 
+#pragma endscop
 }
 
 
