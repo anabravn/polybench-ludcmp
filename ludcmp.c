df831f64e00a180b5317d589dee386546d463af8
anabravn <bravinheloisa@gmail.com>
2023-08-23 13:47:45 -0300

    indent

--- ludcmp.c
+++ ludcmp.c
@@ -71,41 +71,37 @@ void kernel_ludcmp(int n,
 
 #pragma scop
   b[0] = 1.0;
-  for (i = 0; i < n; i++)
-    {
-      for (j = i+1; j <= n; j++)
-        {
-	  w = A[j][i];
-	  for (k = 0; k < i; k++)
-	    w = w- A[j][k] * A[k][i];
-	  A[j][i] = w / A[i][i];
-        }
-      for (j = i+1; j <= n; j++)
-        {
-	  w = A[i+1][j];
-	  for (k = 0; k <= i; k++)
-	    w = w  - A[i+1][k] * A[k][j];
-	  A[i+1][j] = w;
-        }
+  for (i = 0; i < n; i++) {
+    for (j = i + 1; j <= n; j++) {
+      w = A[j][i];
+      for (k = 0; k < i; k++)
+        w = w - A[j][k] * A[k][i];
+      A[j][i] = w / A[i][i];
     }
+    for (j = i + 1; j <= n; j++) {
+      w = A[i + 1][j];
+      for (k = 0; k <= i; k++)
+       w = w - A[i + 1][k] * A[k][j];
+      A[i + 1][j] = w;
+    }
+  }
   y[0] = b[0];
-  for (i = 1; i <= n; i++)
-    {
-      w = b[i];
-      for (j = 0; j < i; j++)
-	w = w - A[i][j] * y[j];
-      y[i] = w;
-    }
+  for (i = 1; i <= n; i++) {
+    w = b[i];
+    for (j = 0; j < i; j++)
+      w = w - A[i][j] * y[j];
+    y[i] = w;
+  }
+  
   x[n] = y[n] / A[n][n];
-  for (i = 0; i <= n - 1; i++)
-    {
-      w = y[n - 1 - (i)];
-      for (j = n - i; j <= n; j++)
-	w = w - A[n - 1 - i][j] * x[j];
-      x[n - 1 - i] = w / A[n - 1 - (i)][n - 1-(i)];
-    }
-#pragma endscop
-
+  
+  for (i = 0; i <= n - 1; i++) {
+    w = y[n - 1 - (i)];
+    for (j = n - i; j <= n; j++)
+      w = w - A[n - 1 - i][j] * x[j];
+      x[n - 1 - i] = w / A[n - 1 - (i)][n - 1 - (i)];
+  }
+ #pragma endscop
 }
 
 
@@ -135,7 +131,6 @@ int main(int argc, char* argv[])
   POLYBENCH_1D_ARRAY_DECL(b, DATA_TYPE, n+1, n+1);
   POLYBENCH_1D_ARRAY_DECL(x, DATA_TYPE, n+1, n+1);
   POLYBENCH_1D_ARRAY_DECL(y, DATA_TYPE, n+1, n+1);
-
 
   /* Initialize array(s). */
   init_array (n,
