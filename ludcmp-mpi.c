6bc313016869af9dd54fda4fcc2c7a61472a8a6d
anabravn <bravinheloisa@gmail.com>
2023-10-05 22:27:21 -0300

    funcionando, vou mimir

--- ludcmp-mpi.c
+++ ludcmp-mpi.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include <mpi.h>
+#include <string.h>
 
 #include "ludcmp.h"
 
@@ -8,7 +9,7 @@ int main(int argc, char **argv) {
     MPI_Comm MPI_COMM_WORKERS;
     float **a, *buffer;
     int process_rank, world_size;
-    int n = 5;
+    int n;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -16,10 +17,25 @@ int main(int argc, char **argv) {
 
     MPI_Comm_split(MPI_COMM_WORLD, process_rank > 0, 
                    process_rank, &MPI_COMM_WORKERS);
+
+    if(!strcmp(argv[1], "-d")) {
+          int i;
+
+          for(i = 0; i < 4; i++) {
+             if(!strcmp(argv[2], dataset_labels[i]))
+                 break;
+          }
+
+          if (i >= 4) {
+                fprintf(stderr, "Erro: Tamanho inválido\n");
+                return 2;
+          }
+
+          n = dataset_sizes[i];
+    }
    
     if (process_rank == 0) {
         a = init_matrix(n);
-        print_matrix(n, a);
     } else {
         a = alloc_matrix(n);
     }
@@ -39,52 +55,61 @@ int main(int argc, char **argv) {
                 buffer[i - start] = a[i][j];
             }
 
-            printf("0 -> sending ");
-            print_array(size, buffer);
-
             for (int i=0; i < world_size-1; i++) {
                 int tmp = (i == world_size-2) ? (size_per_proc + rem) : size_per_proc;
 
-                printf("0 -> sending to %d -> ", i + 1);
                 MPI_Send(&buffer[i*size_per_proc], tmp, MPI_FLOAT, 
                         i + 1, i, MPI_COMM_WORLD); 
-                print_array(tmp, &buffer[i*size_per_proc]);
             }
        }
 
        if (process_rank != 0) {
-           for (int i=0; i < world_size-1; i++) {
-               int tmp = (i == world_size-2) ? (size_per_proc + rem) : size_per_proc;
+           for (int proc=0; proc < world_size-1; proc++) {
+               int tmp = (proc == world_size-2) ? (size_per_proc + rem) : size_per_proc;
+               int x = start + size_per_proc * proc;
+               int y = x + tmp;
 
-               if(process_rank - 1 == i) {
-                    MPI_Recv(&buffer[i*size_per_proc], tmp, MPI_FLOAT, 
+               if(process_rank - 1 == proc) {
+                    MPI_Recv(&buffer[proc*size_per_proc], tmp, MPI_FLOAT, 
                         0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
 
-                    printf("%d -> received ", process_rank);
-                    print_array(tmp, &buffer[i*size_per_proc]);
+                    for(int i = x; i < y; i++)  {
+                        int k, j = d - i;
+                        float w;
 
-                    printf("%d -> broadcasting results ", process_rank);
-                } else {
-                    printf("%d -> receiving results from %d -> ", process_rank, i + 1);
+                        a[i][j] = buffer[i - start];
+
+                        if (i > j) {
+                            w = a[i][j];
+                            for (k = 0; k < j; k++) {
+                              w -= a[i][k] * a[k][j];
+                            }
+                            a[i][j] = w / a[j][j];
+                        } else {
+                             w = a[i][j];
+                           for (k = 0; k < i; k++) {
+                                  w -= a[i][k] * a[k][j];
+                               }
+                           a[i][j] = w;
+                        }
+
+                        buffer[i - start] = a[i][j];
+                    }
+                } 
+
+                MPI_Bcast(&buffer[proc*size_per_proc], tmp, MPI_FLOAT, proc, MPI_COMM_WORKERS);
+
+                for (int i=start; i <= end; i++) {
+                    int j = d - i;
+                    a[i][j] = buffer[i - start];
                 }
-
-                MPI_Bcast(&buffer[i*size_per_proc], tmp, MPI_FLOAT, i, MPI_COMM_WORKERS);
-                print_array(tmp, &buffer[i*size_per_proc]);
-            }
-
-            printf("%d -> diagonal %d -> ", process_rank, d);
-            print_array(size, buffer);
-
-            for (int i=start; i <= end; i++) {
-                int j = d - i;
-                a[i][j] = buffer[i - start];
-            }
-         }
+             }
+        }
        
          MPI_Barrier(MPI_COMM_WORKERS);
     }
 
-    if(process_rank == world_size - 1)
+    if(process_rank == world_size - 1 && n <= dataset_sizes[0])
         print_matrix(n, a);
 
     MPI_Finalize();
